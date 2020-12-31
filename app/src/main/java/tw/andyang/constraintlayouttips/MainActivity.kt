902c5101c24dfbeef37de4c13d496049cd9c3278
andyang <yang.bng86@gmail.com>
2020-12-31 09:23:42 +0800

    init commit

---
+++ app/src/main/java/tw/andyang/constraintlayouttips/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.andyang.constraintlayouttips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
